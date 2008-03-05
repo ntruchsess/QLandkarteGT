3f495d6f07c3be7057d9783aba1b4aed8465d3a6
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2008-03-05 20:49:43 +0000

    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@282 d0c010d8-bb82-477e-a075-112140230451

--- src/CCreateMapGeoTiff.h
+++ src/CCreateMapGeoTiff.h
@@ -1,31 +1,28 @@
 /**********************************************************************************************
+    Copyright (C) 2008 Oliver Eichler oliver.eichler@gmx.de
 
-  DSP Solutions
-  Ingenieure Kellermann, Voigt, Hoepfl, Eichler und Weidner, Partnerschaft
-  http://www.dspsolutions.de/
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-  Author:      Not defined
-  Email:       Not defined
-  Phone:       Not defined
-  FAX:         +49-941-83055-79
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  File:        CCreateMapGeoTiff.h
-
-  Module:
-
-  Description:
-
-  Created:     03/05/2008
-
-  (C) 2008
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
 
 **********************************************************************************************/
 #ifndef CCREATEMAPGEOTIFF_H
 #define CCREATEMAPGEOTIFF_H
 
 #include <QWidget>
+#include "ui_ICreateMapGeoTiff.h"
 
-class CCreateMapGeoTiff : public QWidget
+class CCreateMapGeoTiff : public QWidget, private Ui::ICreateMapGeoTiff
 {
     Q_OBJECT;
     public:
