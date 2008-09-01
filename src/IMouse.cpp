417339b829d0c1164049743c6c586a232721e0aa
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2008-09-01 12:30:47 +0000

    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@675 d0c010d8-bb82-477e-a075-112140230451

--- src/IMouse.cpp
+++ src/IMouse.cpp
@@ -86,7 +86,6 @@ void IMouse::drawSelWpt(QPainter& p)
 
         p.setPen(QColor(100,100,255,200));
         p.setBrush(QColor(255,255,255,200));
-//         p.drawEllipse(QPoint(u, v), 35, 35);
         p.drawEllipse(u - 35, v - 35, 70, 70);
 
         p.save();
