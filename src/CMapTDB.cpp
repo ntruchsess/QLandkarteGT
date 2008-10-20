e544a56d060a868a1c0eccde48cfcba48cb956ce
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2008-10-20 20:12:24 +0000

    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@818 d0c010d8-bb82-477e-a075-112140230451

--- src/CMapTDB.cpp
+++ src/CMapTDB.cpp
@@ -175,7 +175,7 @@ CMapTDB::CMapTDB(const QString& key, const QString& filename, CCanvas * parent)
     polylineProperties[0x0C] = polyline_property(0x0C, "#FFFFFF",   2, Qt::SolidLine);
     polylineProperties[0x14] = polyline_property(0x14, "#FFFFFF",   2, Qt::DotLine);
     polylineProperties[0x15] = polyline_property(0x15, "#000080",   2, Qt::SolidLine);
-    polylineProperties[0x16] = polyline_property(0x16, "#808080",   2, Qt::SolidLine);
+    polylineProperties[0x16] = polyline_property(0x16, "#E0E0E0",   2, Qt::SolidLine);
     polylineProperties[0x18] = polyline_property(0x18, "#0000ff",   2, Qt::SolidLine);
     polylineProperties[0x19] = polyline_property(0x19, "#00ff00",   2, Qt::SolidLine);
     polylineProperties[0x1A] = polyline_property(0x1A, "#000000",   2, Qt::SolidLine);
@@ -197,7 +197,6 @@ CMapTDB::CMapTDB(const QString& key, const QString& filename, CCanvas * parent)
     polylineProperties[0x2A] = polyline_property(0x2A, "#000000",   2, Qt::SolidLine);
     polylineProperties[0x2B] = polyline_property(0x2B, "#000000",   2, Qt::SolidLine);
 
-    polygonProperties[0x00] = polygon_property(0x00, Qt::magenta,   "#000000", Qt::NoBrush);
     polygonProperties[0x01] = polygon_property(0x01, Qt::NoPen,     "#d2c0c0", Qt::SolidPattern);
     polygonProperties[0x02] = polygon_property(0x02, Qt::NoPen,     "#fbeab7", Qt::SolidPattern);
     polygonProperties[0x03] = polygon_property(0x03, Qt::NoPen,     "#a4b094", Qt::SolidPattern);
@@ -750,8 +749,6 @@ static quint16 others[]  = { 0x00, /*0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x08, 0
 
 void CMapTDB::drawPolylines(QPainter& p, polytype_t& lines)
 {
-    int n;
-    const int N = sizeof(streets)/sizeof(quint16);
 
 //     QSet<quint16> setStreets;
 //     setStreets << 0x01 << 0x02 << 0x03 << 0x04 << 0x05 << 0x06 << 0x08 << 0x09 << 0x0A << 0x0B << 0x0C << 0x14;
@@ -798,6 +795,8 @@ void CMapTDB::drawPolylines(QPainter& p, polytype_t& lines)
 //         ++item;
 //     }
 
+    int n;
+    const int N = sizeof(streets)/sizeof(quint16);
 
     for(n = 0; n < N; ++n){
         quint16 type = streets[n];
@@ -889,29 +888,48 @@ void CMapTDB::drawPolylines(QPainter& p, polytype_t& lines)
     }
 }
 
+static quint16 order[] = {  0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+                            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+                            0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+                            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+                            0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
+                            0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
+                            0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+                            0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F
+                         };
+
 
 void CMapTDB::drawPolygons(QPainter& p, polytype_t& lines)
 {
-    polytype_t::iterator item = lines.begin();
-    p.setPen(Qt::magenta);
-    while (item != lines.end()) {
-        double * u      = item->u.data();
-        double * v      = item->v.data();
-        const int size  = item->u.size();
 
-        convertRad2Pt(u,v,size);
+    int n;
+    const int N = sizeof(order)/sizeof(quint16);
 
-        QPolygonF line(size);
+    for(n = 0; n < N; ++n){
+        quint16 type = order[n];
 
-        for(int i = 0; i < size; ++i){
-            line[i].setX(*u++);
-            line[i].setY(*v++);
+        p.setPen(polygonProperties[type].pen);
+        p.setBrush(polygonProperties[type].brush);
+
+        polytype_t::iterator item = lines.begin();
+        while (item != lines.end()) {
+            if(item->type == type){
+                double * u      = item->u.data();
+                double * v      = item->v.data();
+                const int size  = item->u.size();
+
+                convertRad2Pt(u,v,size);
+
+                QPolygonF line(size);
+
+                for(int i = 0; i < size; ++i){
+                    line[i].setX(*u++);
+                    line[i].setY(*v++);
+                }
+
+                p.drawPolygon(line);
+            }
+            ++item;
         }
-
-        p.setPen(polygonProperties[item->type].pen);
-        p.setBrush(polygonProperties[item->type].brush);
-        p.drawPolygon(line);
-        ++item;
     }
-
 }
