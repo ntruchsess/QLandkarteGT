a0081c3e1662b1a93b6a55f3cfc3ceb5b15a4abd
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2009-01-22 07:55:05 +0000

    fix plugin path
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@1108 d0c010d8-bb82-477e-a075-112140230451

--- src/CDlgConfig.cpp
+++ src/CDlgConfig.cpp
@@ -16,7 +16,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 **********************************************************************************************/
-
+#include "config.h"
 #include "CDlgConfig.h"
 #include "CResources.h"
 #include "IDevice.h"
