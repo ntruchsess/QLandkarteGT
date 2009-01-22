b94c380a65e764317d3808f53d267ea23f2b2733
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2009-01-22 14:23:08 +0000

    bug fix: remove quotation marks as they are obsolete and break the code
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@1109 d0c010d8-bb82-477e-a075-112140230451

--- config.h.cmake
+++ config.h.cmake
@@ -1,14 +1,14 @@
-#cmakedefine PACKAGE "${APPLICATION_NAME}"
-#cmakedefine VERSION "${APPLICATION_VERSION}"
-#cmakedefine LOCALEDIR "${LOCALE_INSTALL_DIR}"
-#cmakedefine DATADIR "${SHARE_INSTALL_PREFIX}"
-#cmakedefine LIBDIR "${LIB_INSTALL_DIR}"
-#cmakedefine PLUGINDIR "${PLUGINDIR}"
-#cmakedefine SYSCONFDIR "${SYSCONFDIR}"
-#cmakedefine BINARYDIR "${BINARYDIR}"
-#cmakedefine SOURCEDIR "${SOURCEDIR}"
+#cmakedefine PACKAGE ${APPLICATION_NAME}
+#cmakedefine VERSION ${APPLICATION_VERSION}
+#cmakedefine LOCALEDIR ${LOCALE_INSTALL_DIR}
+#cmakedefine DATADIR ${SHARE_INSTALL_PREFIX}
+#cmakedefine LIBDIR ${LIB_INSTALL_DIR}
+#cmakedefine PLUGINDIR ${PLUGINDIR}
+#cmakedefine SYSCONFDIR ${SYSCONFDIR}
+#cmakedefine BINARYDIR ${BINARYDIR}
+#cmakedefine SOURCEDIR ${SOURCEDIR}
 
-#cmakedefine SHARED_LIB_EXT "${SHARED_LIB_EXT}"
+#cmakedefine SHARED_LIB_EXT ${SHARED_LIB_EXT}
 
 #cmakedefine HAVE_STDINT_H 1
 #cmakedefine HAVE_INTTYPES_H 1
