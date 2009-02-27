07fcb5dc0a338d366494ccc05a339aa6789f3391
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2009-02-27 21:58:09 +0000

    change projection to some sane settings to allow DEM
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@1204 d0c010d8-bb82-477e-a075-112140230451

--- src/CMapOSM.cpp
+++ src/CMapOSM.cpp
@@ -35,7 +35,11 @@ CMapOSM::CMapOSM(CCanvas * parent)
 {
     osmTiles = new COsmTilesHash(this);
     connect(osmTiles,SIGNAL(newImageReady(QImage,bool)),this,SLOT(newImageReady(QImage,bool)));
-    pjsrc = pj_init_plus("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs");
+    pjsrc = pj_init_plus("+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs ");
+
+    char * ptr = pj_get_def(pjsrc,0);
+    qDebug() << "OSM:" << ptr;
+    if(ptr) free(ptr);
 
     QSettings cfg;
     QString pos     = cfg.value("osm/topleft","").toString();
