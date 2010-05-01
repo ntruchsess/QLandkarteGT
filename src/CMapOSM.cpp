27ac8469affe8f97c4b9d2425a4e3e3606c6a0ee
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2010-05-01 10:02:03 +0000

    private stuff
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@2084 d0c010d8-bb82-477e-a075-112140230451

--- src/CMapOSM.cpp
+++ src/CMapOSM.cpp
@@ -44,6 +44,11 @@ CMapOSM::CMapOSM(CCanvas * parent)
     tileList << qMakePair(QString("OpenCycleMap"),QString("andy.sandbox.cloudmade.com/tiles/cycle/%1/%2/%3.png"));
     //    tileList << qMakePair(QString("topo.geofabrik.de CC-NC-SA"),QString("topo.geofabrik.de/trails/%1/%2/%3.png"));
     tileList << qMakePair(QString("OpenPisteMap"),QString("openpistemap.org/tiles/contours/%1/%2/%3.png"));
+#if PRIVATE
+    tileList << qMakePair(QString("Outdooractive DE"),QString("t0.outdooractive.com/portal/map/%1/%2/%3.png"));
+    tileList << qMakePair(QString("Outdooractive AU"),QString("t0.outdooractive.com/austria/map/%1/%2/%3.png"));
+    tileList << qMakePair(QString("Outdooractive IT"),QString("t0.outdooractive.com/suedtirol/map/%1/%2/%3.png"));
+#endif
 
     cb = new QComboBox(theMainWindow->getCanvas());
 
