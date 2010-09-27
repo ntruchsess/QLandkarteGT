de03f07d21e07e6a8d83a8ece000dc2c1e3c299a
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2010-09-27 12:05:27 +0000

    fix non transparent menu for qt platique
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@2379 d0c010d8-bb82-477e-a075-112140230451

--- src/CMegaMenu.cpp
+++ src/CMegaMenu.cpp
@@ -157,6 +157,7 @@ void CMegaMenu::initStyleOption(QStyleOptionMenuItem *option, const QAction *act
 
     option->initFrom(this);
     option->palette = palette();
+    option->palette.setBrush(QPalette::Normal, QPalette::Window, QColor(0,0,0,0));
     option->palette.setBrush(QPalette::Normal, QPalette::Button, Qt::NoBrush);
     option->palette.setBrush(QPalette::Inactive, QPalette::Button, Qt::NoBrush);
     option->state = QStyle::State_None;
