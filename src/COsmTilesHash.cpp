9d246c895279832497a0f6cda39a3af26c22dae5
mfeld <mfeld@d0c010d8-bb82-477e-a075-112140230451>
2011-11-16 19:04:16 +0000

    fixes typing error
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@3086 d0c010d8-bb82-477e-a075-112140230451

--- src/COsmTilesHash.cpp
+++ src/COsmTilesHash.cpp
@@ -5,7 +5,7 @@
 //C- This software is subject to, and may be distributed under, the
 //C- GNU General Public License, either version 2 of the license,
 //C- or (at your option) any later version. The license should have
-//C- acOSM_COMpanied the software or you may obtain a copy of the license
+//C- accompanied the software or you may obtain a copy of the license
 //C- from the Free Software Foundation at http://www.fsf.org .
 //C-
 //C- This program is distributed in the hope that it will be useful,
