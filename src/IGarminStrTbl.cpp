9fbccd0418ce9696249c006ddea59798a40d0fe5
kiozen <kiozen@d0c010d8-bb82-477e-a075-112140230451>
2013-06-24 09:17:47 +0000

    fix chrash with Garmin maps using unknown codepage for labels
    git-svn-id: http://svn.code.sf.net/p/qlandkartegt/code/QLandkarteGT/trunk@3640 d0c010d8-bb82-477e-a075-112140230451

--- src/IGarminStrTbl.cpp
+++ src/IGarminStrTbl.cpp
@@ -5,12 +5,12 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-    
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -50,9 +50,14 @@ IGarminStrTbl::IGarminStrTbl(const quint16 codepage, const quint8 mask, QObject 
         {
             codec = QTextCodec::codecForName("IBM 850");
         }
+        else if(codepage == 65001)
+        {
+            codec = QTextCodec::codecForName("UTF-8");
+        }
         else
         {
             qDebug() << "unknown codepage:" << codepage << "0x" << hex << codepage;
+            codec = QTextCodec::codecForName("Latin1");
         }
     }
 
